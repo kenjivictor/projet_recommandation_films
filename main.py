93515e1e240d518de5f0651b24131c5a4a015c70
kenjivictor <kenji.a.victor@gmail.com>
2025-12-03 19:37:27 +0100

    update main page + update python version

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from projet-recommandation-films!")
+import streamlit as st
 
 
-if __name__ == "__main__":
-    main()
+st.title("🍿NANTES-FLIX🎥")
+st.text("🎞️ Bienvenue sur notre plateforme de recommandation de films à Nantes!")
+st.text("🚧 Notre site en cours de construction 🚧")
